23b058576a38507ed1ae577b813b99d1d06c2eea
Andre Luis Ribeiro de Souza <andre09999@live.com>
2024-03-07 11:56:39 -0300

    excluir produtos

--- src/complements/produtos.js
+++ src/complements/produtos.js
@@ -1,18 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { HiOutlinePencilSquare } from "react-icons/hi2";
+import { MdDeleteForever } from "react-icons/md";
+
 import { useNavigate } from 'react-router-dom';
 
 const Produtos = ({value, value1, value2}) => {
   const [data, setData] = useState([]);
   const [dataFilter, setDatFiltera] = useState([]);
   const history = useNavigate();
+  const delet = (id) => {
+    const token = localStorage.getItem('token').replace(/[""]/g, '');
 
+    axios.delete(`https://apiproducts-rij8.onrender.com/product/${id.id}`,{
+      headers: {
+        Authorization: `${token}` 
+      }
+    })
+    .then(response => {
+      setData(response.data)
+     
+    }).catch(error => {
+     
+      console.log(error.response.data);
+    });
+  }
   useEffect(() => {
     axios.get('https://apiproducts-rij8.onrender.com/product')
     .then(response => {
       setData(response.data)
-     
     }).catch(error => {
      
       console.log(error.response.data);
@@ -40,7 +56,8 @@ const Produtos = ({value, value1, value2}) => {
       {
         dataFilter.map((a, i) => (
           <div key={i} className='card'>
-            <button onClick={()=> edit(a)} className='button_card'> <HiOutlinePencilSquare color='white' size='2em'/>  </button>
+            <button onClick={() => edit(a)} className='button_card'> <HiOutlinePencilSquare color='white' size='2em' />  </button>
+            <button onClick={()=> delet(a)} className='button_card'> <MdDeleteForever  color='white' size='2em'/>  </button>
             <p>Nome: {a.name}</p>
             <p>Marca: {a.brand}</p>
             <p>Modelo: {a.model}</p>
